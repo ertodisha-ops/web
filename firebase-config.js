0d1460b1d00fda8e40da7a35235881fbd68715a0
ertodisha <ertodisha@gmail.com>
2026-02-05 18:54:02 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 // TODO: PASTE YOUR FIREBASE KEYS HERE
 const firebaseConfig = {
     apiKey: "AIzaSyB54YuV-seHAxRhTia0nuuDmE5Sh2ta61w",
-  authDomain: "ertquiz.firebaseapp.com",
-  projectId: "ertquiz",
-  storageBucket: "ertquiz.firebasestorage.app",
-  messagingSenderId: "204115314828",
-  appId: "1:204115314828:web:148207f1b134f6d5fd13a9"
+    authDomain: "ertquiz.firebaseapp.com",
+    projectId: "ertquiz",
+    storageBucket: "ertquiz.firebasestorage.app",
+    messagingSenderId: "204115314828",
+    appId: "1:204115314828:web:148207f1b134f6d5fd13a9"
 };
 
 const app = initializeApp(firebaseConfig);
